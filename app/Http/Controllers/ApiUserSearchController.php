1fc1459b061a601b7ddf8f7fdc4759184c24ec4c
Hunter Do <hunter.do.15902@gmail.com>
2024-08-31 17:26:07 +0700

    User searching completed

---
+++ app/Http/Controllers/ApiUserSearchController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Helper\ResponseHelper;
+
+class ApiUserSearchController extends Controller
+{
+    public function searchByName(Request $request, ?string $name = null)
+    {
+        $currUser = $request->user();
+
+        if($name == null){
+            $msg = 'Please enter a name.';
+            return ResponseHelper::error(message: $msg);
+        }
+
+        $result = User::where('name', 'like', '%' . $name . '%')->where('id', '!=', $currUser->id);
+        $result = $result->get();
+
+        if($result->isEmpty()){
+            $msg = 'No users found.';
+            return ResponseHelper::success(message: $msg);
+        }
+
+        return ResponseHelper::success(data: $result);
+    }
+
+    public function searchByCode(Request $request, ?string $code = null)
+    {
+        $currUser = $request->user();
+
+        if($code == null){
+            $msg = 'Please enter a user code.';
+            return ResponseHelper::error(message: $msg);
+        }
+
+        $result = User::where('user_code', $code)->where('user_code', '!=', $currUser->user_code);
+        $result = $result->get();
+
+        if($result->isEmpty()){
+            $msg = 'No users found.';
+            return ResponseHelper::success(message: $msg);
+        }
+
+        return ResponseHelper::success(data: $result);
+    }
+}
