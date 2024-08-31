934349f00890cb7fd0f6e364c4c4ca7c9f0b407c
Hunter Do <hunter.do.15902@gmail.com>
2024-08-31 21:04:59 +0700

    Add try-catch blocks to ApiUserSearchController

--- app/Http/Controllers/ApiUserSearchController.php
+++ app/Http/Controllers/ApiUserSearchController.php
@@ -10,41 +10,49 @@ class ApiUserSearchController extends Controller
 {
     public function searchByName(Request $request, ?string $name = null)
     {
-        $currUser = $request->user();
+        try {
+            $currUser = $request->user();
 
-        if($name == null){
-            $msg = 'Please enter a name.';
-            return ResponseHelper::error(message: $msg);
+            if($name == null){
+                $msg = 'Please enter a name.';
+                return ResponseHelper::error(message: $msg);
+            }
+    
+            $result = User::where('name', 'like', '%' . $name . '%')->where('id', '!=', $currUser->id);
+            $result = $result->get();
+    
+            if($result->isEmpty()){
+                $msg = 'No users found.';
+                return ResponseHelper::success(message: $msg);
+            }
+    
+            return ResponseHelper::success(data: $result);
+        } catch (\Throwable $th) {
+            return ResponseHelper::error(message: $th->getMessage());
         }
-
-        $result = User::where('name', 'like', '%' . $name . '%')->where('id', '!=', $currUser->id);
-        $result = $result->get();
-
-        if($result->isEmpty()){
-            $msg = 'No users found.';
-            return ResponseHelper::success(message: $msg);
-        }
-
-        return ResponseHelper::success(data: $result);
     }
 
     public function searchByCode(Request $request, ?string $code = null)
     {
-        $currUser = $request->user();
+        try {
+            $currUser = $request->user();
 
-        if($code == null){
-            $msg = 'Please enter a user code.';
-            return ResponseHelper::error(message: $msg);
+            if($code == null){
+                $msg = 'Please enter a user code.';
+                return ResponseHelper::error(message: $msg);
+            }
+    
+            $result = User::where('user_code', $code)->where('user_code', '!=', $currUser->user_code);
+            $result = $result->get();
+    
+            if($result->isEmpty()){
+                $msg = 'No users found.';
+                return ResponseHelper::success(message: $msg);
+            }
+    
+            return ResponseHelper::success(data: $result);
+        } catch (\Throwable $th) {
+            return ResponseHelper::error(message: $th->getMessage());
         }
-
-        $result = User::where('user_code', $code)->where('user_code', '!=', $currUser->user_code);
-        $result = $result->get();
-
-        if($result->isEmpty()){
-            $msg = 'No users found.';
-            return ResponseHelper::success(message: $msg);
-        }
-
-        return ResponseHelper::success(data: $result);
     }
 }
