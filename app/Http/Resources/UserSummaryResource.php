f46be65d7a8d4d1c7f2ce7de01b68a39b2b59771
Duykhanh29 <duykhanh0220@gmail.com>
2024-09-05 11:57:06 +0700

    change response in search user API

--- app/Http/Resources/UserSummaryResource.php
+++ app/Http/Resources/UserSummaryResource.php
@@ -18,6 +18,7 @@ class UserSummaryResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'avatar' => $this->avatar,
+            'user_code' => $this->user_code
         ];
     }
 }
