8b706eb4ddbf5fc31e13a68f0d40a4b2af2f4783
Hunter Do <hunter.do.15902@gmail.com>
2024-09-07 11:51:33 +0700

    post report completed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Report;
 use App\Helper\ImageHelper;
 use App\Enum\SharedPostType;
 use Illuminate\Http\Request;
@@ -187,7 +188,6 @@ class PostController extends Controller
         }
     }
 
-
     public function getUserLike(Request $request, $id)
     {
         try {
@@ -227,8 +227,27 @@ class PostController extends Controller
         }
     }   
 
-    // public function postReport(Request $request, $id)
-    // {
-    //     $post = Post::findOrFail($id);
-    // }
+    public function postReport(Request $request, $id)
+    {
+        try {
+            $post = Post::findOrFail($id);
+            $user = $request->user();
+            $data = $request->all();
+
+            $report = Report::create([
+                'post_id' => $post->id,
+                'reason' => $data['reason'],
+                'reported_user' => $post->user_id,
+                'user_reporting' => $user->id,
+            ]);
+
+            $report = $report->fresh();
+
+            $msg = 'Reported successfully!';
+
+            return ResponseHelper::success(message: $msg, data: $report);
+        } catch (\Throwable $th) {
+            ResponseHelper::error(message: $th->getMessage());
+        }
+    }
 }
