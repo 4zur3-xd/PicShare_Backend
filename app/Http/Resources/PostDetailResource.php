763b42507db7a416a0ceebbe4c8b82561dc6ecb8
Duykhanh29 <duykhanh0220@gmail.com>
2024-09-14 21:42:27 +0700

    update: return user views and user likes in post responses

--- app/Http/Resources/PostDetailResource.php
+++ app/Http/Resources/PostDetailResource.php
@@ -13,7 +13,22 @@ class PostDetailResource extends JsonResource
      * @return array<string, mixed>
      */
     public function toArray(Request $request): array
-    {
+    {   
+         $userLikes = $this->resource->likes()->with('user')->get()->map(function ($like) {
+            return [
+                'id' => $like->user_id,
+                'name' => $like->user->name,
+                'url_avatar' => $like->user->url_avatar,
+            ];
+        });
+
+        $userViews = $this->resource->userViews()->with('user')->get()->map(function ($view) {
+            return [
+                'id' => $view->user_id,
+                'name' => $view->user->name,
+                'url_avatar' => $view->user->url_avatar,
+            ];
+        });
         return [
             'id' => $this->id,
             'url_image' => $this->url_image,
@@ -27,6 +42,8 @@ class PostDetailResource extends JsonResource
             'type'=> $this->type,
             'user' => new UserSummaryResource($this->whenLoaded('user')),
             'listCmt' => CommentResource::collection($this->whenLoaded('comments')),
+            'user_likes' => $userLikes, 
+            'user_views' => $userViews, 
         ];
     }
 }
