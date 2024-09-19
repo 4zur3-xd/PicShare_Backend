1b67c5707bdb35a2beb6f71c3ca52b04c60511a2
Hunter Do <hunter.do.15902@gmail.com>
2024-09-19 13:29:28 +0700

    home page completed, minor changes in login page

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,12 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// Route::get('/test', function(){
+//     return view('home');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
