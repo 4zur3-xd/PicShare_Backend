16670ec93380998d95e54d48006df4eb28247f43
Hunter Do <hunter.do.15902@gmail.com>
2024-09-30 12:52:31 +0700

    login button changed (login page), add an if statement in case there is no reports in database (reports page)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
             </label>
         </div> --}}
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="mt-4">
             {{-- @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
@@ -43,7 +43,7 @@
                 Create an account here!
             </a>
 
-            <x-primary-button class="ms-3">
+            <x-primary-button style="display: block; width: 100%; margin-top: 25px;">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
